0232832cc0372b87d573bc5dbb8a0c0e6c745daf
Van5sh <vansh05dhir@gmail.com>
2026-01-08 20:37:22 +0530

    feat:centre

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,10 +2,11 @@ import Arrow1 from "../../public/svgs/Arrow1";
 import Arrow2 from "../../public/svgs/Arrow2";
 import Arrow3 from "../../public/svgs/Arrow3";
 import Arrow4 from "../../public/svgs/Arrow4";
+import Centre from "../../public/svgs/Centre";
 
 const Projects = () => {
     return (
-        <div className="flex flex-col items-center py-16 w-full">
+        <div className="flex flex-col items-center p-16 w-full">
             <h1 className="text-3xl font-bold mb-12">Projects</h1>
             <div className="relative w-full max-w-5xl h-[60vh]">
                 <div className="absolute inset-0 flex justify-between items-center px-12">
@@ -13,6 +14,7 @@ const Projects = () => {
                         <Arrow4 />
                         <Arrow3 />
                     </div>
+                    <Centre/>
                     <div className="flex flex-col gap-8">
                         <Arrow1 />
                         <Arrow2 />
